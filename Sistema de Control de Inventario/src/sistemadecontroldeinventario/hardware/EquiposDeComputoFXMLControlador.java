c704c7e757c31c08999b7aaba433efdd6fb30404
Froylan-Aarkan <77468347+Froylan-Aarkan@users.noreply.github.com>
2023-10-20 10:17:01 -0600

    Proyecto Base v3

---
+++ Sistema de Control de Inventario/src/sistemadecontroldeinventario/hardware/EquiposDeComputoFXMLControlador.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package sistemadecontroldeinventario.hardware;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author froyl
+ */
+public class EquiposDeComputoFXMLControlador implements Initializable {
+
+    @FXML
+    private TableView<?> tvEquiposComputo;
+    @FXML
+    private TextField tfBusqueda;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cerrarVentana(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrarEquipoComputo(ActionEvent event) {
+    }
+
+    @FXML
+    private void modificarEquipoComputo(ActionEvent event) {
+    }
+
+    @FXML
+    private void consultarEquipoComputo(ActionEvent event) {
+    }
+
+    @FXML
+    private void eliminarEquipoComputo(ActionEvent event) {
+    }
+    
+}
