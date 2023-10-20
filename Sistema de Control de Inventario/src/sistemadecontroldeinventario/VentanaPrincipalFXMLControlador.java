22df810120fac1bd82d0a8213da10e2eb321038d
Froylan-Aarkan <77468347+Froylan-Aarkan@users.noreply.github.com>
2023-10-20 12:48:38 -0600

    Proyecto Base v3

--- Sistema de Control de Inventario/src/sistemadecontroldeinventario/VentanaPrincipalFXMLControlador.java
+++ Sistema de Control de Inventario/src/sistemadecontroldeinventario/VentanaPrincipalFXMLControlador.java
@@ -4,9 +4,21 @@
  */
 package sistemadecontroldeinventario;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sistemadecontroldeinventario.hardware.EquiposDeComputoFXMLControlador;
 
 /**
  * FXML Controller class
@@ -16,6 +28,8 @@ import javafx.fxml.Initializable;
 public class VentanaPrincipalFXMLControlador implements Initializable {
     
     private String cargoUsuario;
+    @FXML
+    private Button btnHardware;
 
     /**
      * Initializes the controller class.
@@ -27,6 +41,31 @@ public class VentanaPrincipalFXMLControlador implements Initializable {
     
     public void inicializarVentana(String cargoUsuario){
         this.cargoUsuario = cargoUsuario;
+    }
+
+    @FXML
+    private void desplegarVentanaEquiposComputo(ActionEvent event) {
+        try {
+            FXMLLoader loaderVentanaEquiposDeComputo = new FXMLLoader(getClass().getResource("hardware/EquiposDeComputoFXML.fxml"));
+            Parent ventanaEquiposDeComputo = loaderVentanaEquiposDeComputo.load();
+            
+            Scene escenarioEquiposDeComputo = new Scene(ventanaEquiposDeComputo);
+            Stage stageEquiposDeComputo = new Stage();
+            stageEquiposDeComputo.setScene(escenarioEquiposDeComputo);
+            stageEquiposDeComputo.initModality(Modality.APPLICATION_MODAL);
+            stageEquiposDeComputo.showAndWait();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(InicioSesionFXMLControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void desplegarVentanaSoftware(ActionEvent event) {
+    }
+
+    @FXML
+    private void desplegarVentanaUsuarios(ActionEvent event) {
     }
     
 }
--- Sistema de Control de Inventario/src/sistemadecontroldeinventario/hardware/EquiposDeComputoFXMLControlador.java
+++ Sistema de Control de Inventario/src/sistemadecontroldeinventario/hardware/EquiposDeComputoFXMLControlador.java
@@ -4,14 +4,24 @@
  */
 package sistemadecontroldeinventario.hardware;
 
+import Modelo.POJO.Hardware;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sistemadecontroldeinventario.InicioSesionFXMLControlador;
+import sistemadecontroldeinventario.VentanaPrincipalFXMLControlador;
 
 /**
  * FXML Controller class
@@ -20,10 +30,19 @@ import javafx.scene.control.TextField;
  */
 public class EquiposDeComputoFXMLControlador implements Initializable {
 
+    private String cargoUsuario;
     @FXML
-    private TableView<?> tvEquiposComputo;
+    private TableView<Hardware> tvEquiposComputo;
     @FXML
     private TextField tfBusqueda;
+    @FXML
+    private TableColumn<Hardware, String> tcMarca;
+    @FXML
+    private TableColumn<Hardware, String> tcModelo;
+    @FXML
+    private TableColumn<Hardware, String> tcNumeroSerie;
+    @FXML
+    private TableColumn<Hardware, String> tcEstado;
 
     /**
      * Initializes the controller class.
@@ -35,6 +54,8 @@ public class EquiposDeComputoFXMLControlador implements Initializable {
 
     @FXML
     private void cerrarVentana(ActionEvent event) {
+        Stage stage = (Stage) tfBusqueda.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
@@ -51,6 +72,10 @@ public class EquiposDeComputoFXMLControlador implements Initializable {
 
     @FXML
     private void eliminarEquipoComputo(ActionEvent event) {
+    }
+    
+    public void inicializarVentana(String cargoUsuario){
+        this.cargoUsuario = cargoUsuario;
     }
     
 }
